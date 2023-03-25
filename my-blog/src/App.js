1b1193bbda2f6bc031197604074b50cfb04d1b27
Sagar Patel <sagarpatel1313@gmail.com>
2023-03-24 20:00:07 -0400

    Add nav bar for basic routing

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,12 +4,13 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>My Awesome Blog</h1>
+        <NavBar />
         <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />} />
