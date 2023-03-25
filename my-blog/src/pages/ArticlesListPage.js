a139618016d96fdc7b1db4e336e6c3d78a41f43d
Sagar Patel <sagarpatel1313@gmail.com>
2023-03-24 21:22:52 -0400

    Adding routing between pages and articles

--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,6 +1,27 @@
+import { Link } from 'react-router-dom';
+import articles from './article-content';
+
+import ArticlesList from '../components/ArticlesList';
+
 const ArticlesListPage = () => {
+
     return (
-        <h1>This is the articles list page</h1>
+        <>
+            <h1>Articles: </h1>
+            <ArticlesList articles={articles} />
+            {/* <div>
+                Alternate way to list all articles
+            </div>
+            <ul>
+                {articles.map(article => (
+                    <li key={article.name}>
+                        <Link to={`/articles/${article.name}`}>
+                            {article.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul> */}
+        </>
     )
 }
 
